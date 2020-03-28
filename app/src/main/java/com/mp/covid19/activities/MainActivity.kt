bce039ddcde8dffbc2b77d8a75e933d3f8456bbf
Gabriel Tiplea <gabriel.tiplea@brivo.com>
2020-03-29 00:53:37 +0200

    Project structure

---
+++ app/src/main/java/com/mp/covid19/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mp.covid19.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mp.covid19.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
