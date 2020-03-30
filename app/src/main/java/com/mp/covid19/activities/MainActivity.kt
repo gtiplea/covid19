18857cbc2569ecdbe0b221776d6585150f160760
SaladBreaker <lucavlad50@gmail.com>
2020-03-30 19:50:27 +0300

    Guiupdate

--- app/src/main/java/com/mp/covid19/activities/MainActivity.kt
+++ app/src/main/java/com/mp/covid19/activities/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.mp.covid19.activities
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.mp.covid19.R
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val startButton: Button = findViewById(R.id.start_button)
+        startButton.setOnClickListener {
+            val intent = Intent(this, FormActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
